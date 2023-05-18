66a67d48d5dff89aaba8f85e32cc28f24ab23ec6
Sergey.Karavanets <dront22rus@mail.ru>
2023-05-18 20:43:24 +0700

    add 'app.config' in app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/mydatabase'
+
 db = SQLAlchemy(app)
 
 
