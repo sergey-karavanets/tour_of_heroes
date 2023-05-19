b73833ceb58e6ad52df6c4252620f62783aeec28
Sergey.Karavanets <dront22rus@mail.ru>
2023-05-19 16:10:34 +0700

    change 'SQLALCHEMY_DATABASE_URI'

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/mydatabase'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/tour_of_heroes'
 db = SQLAlchemy(app)
 
 
