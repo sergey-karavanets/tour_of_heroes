65bc788a8a0abf623a95626bf362feee7f30eec5
Sergey.Karavanets <dront22rus@mail.ru>
2023-05-19 16:10:58 +0700

    add Heroes model with heroes table

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhos
 db = SQLAlchemy(app)
 
 
+class Heroes(db.Model):
+    __tablename__ = 'heroes'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<{self.id}:{self.name}>'
+
+
 @app.route('/')
 def index():
     return 'Hello World'
